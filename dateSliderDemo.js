04566da1533e34ab9c95f0f44887a1aba35ffc9d
calexity <alexa@carbonfive.com>
2014-08-23 21:44:53 -0700

    new sidebar

--- dateSliderDemo.js
+++ dateSliderDemo.js
@@ -7,26 +7,6 @@
 		options: {},
 		_title: "Date values",
 		_name: "dateRangeSlider",
-		
-		var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
-  $("#dateRulersExample").dateRangeSlider({
-    bounds: {min: new Date(2012, 0, 1), max: new Date(2012, 11, 31, 12, 59, 59)},
-    defaultValues: {min: new Date(2012, 1, 10), max: new Date(2012, 4, 22)},
-    scales: [{
-      first: function(value){ return value; },
-      end: function(value) {return value; },
-      next: function(value){
-        var next = new Date(value);
-        return new Date(next.setMonth(value.getMonth() + 1));
-      },
-      label: function(value){
-        return months[value.getMonth()];
-      },
-      format: function(tickContainer, tickStart, tickEnd){
-        tickContainer.addClass("myCustomClass");
-      }
-    }]
-  });
 
 		_createInputs: function(){
 			$.ui.sliderDemo.prototype._createInputs.apply(this, []);
